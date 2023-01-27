3dc4e1ab4fb690d7c21ec7dafc864133bb1d2f07
Omusek <75886@office.wsm.warszawa.pl>
2023-01-27 06:55:25 +0100

    explosion
    add explosion
    
    Co-Authored-By: Karina <98776246+StarryMartlet@users.noreply.github.com>

--- Assets/Scripts/Explosive.cs
+++ Assets/Scripts/Explosive.cs
@@ -12,13 +12,14 @@ public class Explosive : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
+/*
         UnityEngine.Debug.Log("====== ");
         UnityEngine.Debug.Log("Name " + this.name);
         UnityEngine.Debug.Log("Masa " + this.GetComponent<Rigidbody2D>().mass);
         UnityEngine.Debug.Log("Name colision" + collision.name);
         UnityEngine.Debug.Log("Masa " + collision.GetComponent<Rigidbody2D>().mass);
         UnityEngine.Debug.Log("====== ");
+*/
 
         if (collision.GetComponent<Rigidbody2D>().mass > this.GetComponent<Rigidbody2D>().mass)
         {
@@ -30,8 +31,10 @@ public class Explosive : MonoBehaviour
 
             collision.GetComponent<Rigidbody2D>().mass++;
             UnityEngine.Debug.Log("Masa " + collision.GetComponent<Rigidbody2D>().mass);
+            UnityEngine.Debug.Log("tag :"+ this.name);
             UnityEngine.Debug.Log("====== ");
             Destroy(this.gameObject, 0);
+            
         }
     }
 }
